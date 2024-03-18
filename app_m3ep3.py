e706e82a7cd9d0f0b954190d3d76c8e83356e615
snow5o0 <163907373+snow5o0@users.noreply.github.com>
2024-03-18 20:28:00 -0300

    Update app_m3ep3.py

--- app_m3ep3.py
+++ app_m3ep3.py
@@ -1,46 +1,48 @@
 from datetime import datetime
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 class M3EP():
     
     def __init__(self):
         pass
-   
-    def read_data(self, filepath, pattern='funceme'):
+    
+    def read_data(self, filepath, pattern='bdmep'):
         if pattern == 'bdmep':
             data = pd.read_csv(filepath, skiprows=10, sep=';', decimal=',') 
-            data['Data Medicao'] = pd.to_datetime(data['Data Medicao'], format='%Y-%m-%d')
-            data.drop('Unnamed: 2', axis=1, inplace=True)
-            data.columns = ['date', 'daily_pr_mm']
-            data.date = pd.to_datetime(data.date)
-            data.set_index('date', inplace=True)
-        elif pattern == 'funceme':
-            data = pd.read_csv(filepath, sep=';', decimal='.') 
-            data['data'] = pd.to_datetime(data['data'], format='%Y-%m-%d')
-            data.drop('id', axis=1, inplace=True)
-            data.drop('posto', axis=1, inplace=True)
-            data.columns = ['daily_pr_mm', 'date']
-            data.date = pd.to_datetime(data.date)
-            data.set_index('date', inplace=True)
-            data.sort_index(inplace=True)
+            if 'Data Medicao' not in data.columns:
+                st.error("Cabeçalho 'Data Medicao' não encontrado no arquivo.")
+                return
+            # Tentar adivinhar o formato da data
+            data['Data Medicao'] = pd.to_datetime(data['Data Medicao'], errors='coerce') 
+            if data['Data Medicao'].isnull().sum() > 0:
+                st.error("Falha ao converter a coluna 'Data Medicao' para formato de data. Verifique se o formato é correto.")
+                return
+            if 'Unnamed: 3' in data.columns:
+                data.drop('Unnamed: 3', axis=1, inplace=True)
+            data.columns = ['Data Medicao', 'PRECIPITACAO TOTAL DIARIO (AUT)(mm)', 'TEMPERATURA MEDIA DIARIA (AUT)(°C)']
+            data.set_index('Data Medicao', inplace=True)
+        else:
+            print('You need to specify how to read a new pattern.')
+            data = None
         self.data_ = data
 
     def remove_zero_pr(self):
         column = self.data_.columns[0]
         return self.data_[self.data_[column] > 0]
-   
+    
     def select_by_date(self, start_date, end_date):
         data = self.data_.loc[start_date:end_date]
         self.data_ = data
-   
+    
     def count_events(self, lower_limit, upper_limit):
-        events = self.data_.query(f'daily_pr_mm >= {lower_limit} & daily_pr_mm < {upper_limit}')
+        events = self.data_.query(f'`PRECIPITACAO TOTAL DIARIO (AUT)(mm)` >= {lower_limit} & `PRECIPITACAO TOTAL DIARIO (AUT)(mm)` < {upper_limit}')
         n_events = len(events)
         return n_events
     
-    def m3ep(self, quantile=.95, filepath=None, pattern='funceme',
+    def m3ep(self, quantile=.95, filepath=None, pattern='inmet',
              start_date=None, end_date=None):
         
         if filepath:
@@ -48,6 +50,9 @@ class M3EP():
             
         if start_date:
             self.select_by_date(start_date, end_date)
+        
+        if self.data_ is None:
+            return
         
         data = self.remove_zero_pr()
         column = data.columns[0]
@@ -58,32 +63,22 @@ class M3EP():
         very_strong = median + 2*std
         strong = median + 1*std
         moderated = median
-        n_events_moderated = self.count_events(moderated, strong)
-        n_events_strong = self.count_events(strong, very_strong)
-        n_events_very_strong = self.count_events(very_strong, data.max()[0]+10)
-        self.result_ = {'moderado': {'limiar': moderated, 
-                                      'n eventos': n_events_moderated},
-                        'forte': {'limiar': strong,
-                                  'n eventos': n_events_strong},
-                        'muito forte': {'limiar': very_strong,
-                                        'n eventos': n_events_very_strong}
+        n_events_moderated = int(self.count_events(moderated, strong))
+        n_events_strong = int(self.count_events(strong, very_strong))
+        n_events_very_strong = int(self.count_events(very_strong, data.max()[0]+10))
+        
+        self.result_ = {
+            'moderado': {'limiar': round(moderated, 2), 'n eventos': n_events_moderated},
+            'forte': {'limiar': round(strong, 2), 'n eventos': n_events_strong},
+            'muito forte': {'limiar': round(very_strong, 2), 'n eventos': n_events_very_strong}
+        }
+        
+        self.events_data_ = {
+            'moderado': data[data[column] >= moderated],
+            'forte': data[data[column] >= strong],
+            'muito forte': data[data[column] >= very_strong]
         }
 
-        # Adicionando gráficos para cada evento extremo
-        self.event_plots_ = {}
-        for threshold in ['moderado', 'forte', 'muito forte']:
-            limiar = self.result_[threshold]['limiar']
-            eventos = data[data[column] >= limiar]
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(x=eventos.index, y=eventos['daily_pr_mm'], mode='lines', name='Precipitação diária'))
-            fig.update_layout(title=f'Eventos Extremos - Limiar {threshold.capitalize()} ({limiar} mm)',
-                              xaxis_title='Data',
-                              yaxis_title='Precipitação (mm)',
-                              showlegend=True)
-            self.event_plots_[threshold] = fig
-
-
-# Criando a aplicação Streamlit
 st.title("Metodologia Estatística dos Eventos Extremos de Precipitação")
 st.header('M3EP')
 
@@ -94,8 +89,9 @@ uploaded_files = st.file_uploader("Carregue aqui seu(s) arquivo(s) com dados de 
                                  accept_multiple_files=True)
 
 st.subheader('1.2 Definir padrão de formatação do arquivo')
-st.write('Este programa foi desenvolvido para trabalhar com arquivos CSV no padrão do FUNCEME.')
-st.write('Atualmente os padrões de formatação aceitos são: CSV FUNCEME')
+st.write('Este programa foi desenvolvido, inicialmente, para trabalhar com arquivos CSV no padrão do BDMEP.')
+st.write('Atualmente os padrões de formatação aceitos são: BDMEP')
+st.write('Caso deseje utilizar outro padrão de arquivo edite o método `read_data` do arquivo `m3ep.py`')
 
 st.subheader('2º Definição do quantil limiar')
 quantile = st.slider('Selecione o quantil extremo que deseja utilizar.', 90, 100, 95) / 100
@@ -114,17 +110,30 @@ for uploaded_file in uploaded_files:
     m3ep.m3ep(filepath=uploaded_file,
               quantile=quantile,
               start_date=start_date,
-              end_date=end_date
-              )
+              end_date=end_date,
+              pattern='bdmep')
     st.write('Resultados do arquivo:', uploaded_file.name)
-    st.write(m3ep.result_)
-    resultados[uploaded_file.name[:-4]] = m3ep.result_
+    formatted_results = {
+        'moderado': {'limiar': f"{m3ep.result_['moderado']['limiar']:.2f}", 'n eventos': m3ep.result_['moderado']['n eventos']},
+        'forte': {'limiar': f"{m3ep.result_['forte']['limiar']:.2f}", 'n eventos': m3ep.result_['forte']['n eventos']},
+        'muito forte': {'limiar': f"{m3ep.result_['muito forte']['limiar']:.2f}", 'n eventos': m3ep.result_['muito forte']['n eventos']}
+    }
+    st.table(formatted_results)  # Visualizador de tabela estilo Excel
+    resultados[uploaded_file.name[:-4]] = formatted_results
 
 # Adicionando gráficos para cada limiar de eventos extremos
-if hasattr(m3ep, 'event_plots_'):
+if hasattr(m3ep, 'events_data_'):
     st.subheader('5º Gráficos de Eventos Extremos')
-    for threshold, plot in m3ep.event_plots_.items():
-        st.plotly_chart(plot, use_container_width=True)
+    for threshold in ['moderado', 'forte', 'muito forte']:
+        limiar = m3ep.result_[threshold]['limiar']
+        eventos = m3ep.events_data_[threshold]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=eventos.index, y=eventos['daily_pr_mm'], mode='lines', name='Precipitação diária'))
+        fig.update_layout(title=f'Eventos Extremos - Limiar {threshold.capitalize()} ({limiar} mm)',
+                          xaxis_title='Data',
+                          yaxis_title='Precipitação (mm)',
+                          showlegend=True)
+        st.plotly_chart(fig, use_container_width=True)
 
 st.subheader('6º Exportar resultados para JSON')
 @st.cache_data
